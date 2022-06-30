2d668655327896ef18dc9a8d27407d28b618eae5
Jim Rapp <james.rapp@sap.com>
2022-06-30 11:31:56 -0600

    base service logic for apim

--- components/srv/orders-service.js
+++ components/srv/orders-service.js
@@ -72,9 +72,39 @@ module.exports = cds.service.impl(async function () {
 
   this.on("READ", "developer", async function (req) {
     console.log("enter dev");
-    const apim = await cds.connect.to("developers");
-    console.log("connected to apim");
-    const developerList = await apim.send('GET', 'Developers');
-    console.log(developerList);  
+    const apimDev = await cds.connect.to("developers");
+    console.log("connected to apimDev");
+    const developerList = await apimDev.send(
+      "GET",
+      "Developers?$select=id,emailId,firstName,lastName"
+    );
+    return developerList;
+  });
+  this.on("READ", "bill", async function (req) {
+    console.log("enter bill");
+    const apimBill = await cds.connect.to("monetize");
+    console.log("connected to apimBill");
+    //TODO make month year and developer_id dynamic
+    const billList = await apimBill.send(
+      "GET",
+      "bills?month=06&year=2022&developer_id=martin.frick@sap.com"
+    );
+    console.log(billList);
+    //TODO cover multiple applications
+    const bills = billList.reduce((acc, curVal) => {
+      return [
+        ...acc,
+        {
+          developerId: curVal.developerId,
+          startDate: curVal.startDate,
+          endDate: curVal.endDate,
+          billValue: curVal.bill.value,
+          appName: curVal.apps[0].appName,
+          appBillValue: curVal.apps[0].bill.value,
+        },
+      ];
+    }, []);
+    console.log(bills);
+    return bills;
   });
 });
