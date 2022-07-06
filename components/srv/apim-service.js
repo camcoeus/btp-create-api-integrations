9ac7b549460c9d876ee14d9bedacac302cbef055
Jim Rapp <james.rapp@sap.com>
2022-07-06 16:50:46 -0600

    refactor bill for multiple apps and products

--- components/srv/apim-service.js
+++ components/srv/apim-service.js
@@ -1,44 +1,54 @@
 const cds = require("@sap/cds");
 
 module.exports = cds.service.impl(async function () {
-    this.on("READ", "developer", async function (req) {
-        console.log("enter dev");
-        const apimDev = await cds.connect.to("developers");
-        console.log("connected to apimDev");
-        const developerList = await apimDev.send(
-          "GET",
-          "Developers?$select=id,emailId,firstName,lastName"
-        );
-        return developerList;
-      });
+  this.on("READ", "developer", async function (req) {
+    console.log("enter developer service");
+    const apimDev = await cds.connect.to("developers");
+    console.log("connected to apim - developer");
+    const developerList = await apimDev.send(
+      "GET",
+      "Developers?$select=id,emailId,firstName,lastName"
+    );
+    return developerList;
+  });
 
-      this.on("READ", "bill", async function (req) {
-        console.log("enter bill");
-        const apimBill = await cds.connect.to("monetize");
-        console.log("connected to apimBill");
-        let billList;
-        //TODO make month year dynamic
-        (req.params && req.params[0]) ?
-        billList = await apimBill.send(
+  this.on("READ", "bill", async function (req) {
+    console.log("enter bill service");
+    const apimBill = await cds.connect.to("monetize");
+    console.log("connected to apim - monetize");
+    let billList;
+    let params = req._.req.query;
+    params.month && params.year && params.developer_id
+      ? (billList = await apimBill.send(
           "GET",
-          `bills?month=06&year=2022&developer_id=${req.params[0].developerId}`
-        ) : billList = [{}];
-        console.log(billList);
-        //TODO cover multiple applications
-        const bills = billList.reduce((acc, curVal) => {
+          `bills?month=${params.month}&year=${params.year}&developer_id=${params.developer_id}`
+        ))
+      : (billList = [{}]);
+    //TODO test multiple applications
+    const apps = billList[0].apps;
+    let appLineItems = apps
+      .map((app) => {
+        let productLineItems = app.ratePlanSubscribed.flat();
+        let pLI = productLineItems.reduce((acc, curVal) => {
           return [
             ...acc,
             {
-              developerId: curVal.developerId,
-              startDate: curVal.startDate,
-              endDate: curVal.endDate,
+              developerId: billList[0].developerId,
+              startDate: billList[0].startDate,
+              endDate: billList[0].endDate,
+              appName: app.appName,
+              appId: app.appId,
+              productName: curVal.productName,
+              ratePlanName: curVal.ratePlanName,
               billValue: curVal.bill.value,
-              appName: curVal.apps[0].appName,
-              appBillValue: curVal.apps[0].bill.value,
+              calls: curVal.calls,
             },
           ];
         }, []);
-        console.log(bills);
-        return bills;
-      });
+        return pLI;
+      })
+      .flat();
+    console.log(appLineItems);
+    return appLineItems;
+  });
 });
